ba1d589696002eefcd567bde6d0cd92b2a09e94e
Cris <cjgalvisc@unal.edu.co>
2022-07-18 23:11:58 +0200

    Express Routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 require("dotenv").config();
 const express = require("express");
-
+const routes = require("./frameworks/expressSpecific/routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const API_PREFIX = process.env.API_PREFIX || "/api/v1";
+const dependencies = require("./config/dependencies");
 
 module.exports = {
   start: () => {
+    // Midlewares
+    app.use(express.json());
+    app.use(
+      express.urlencoded({
+        extended: true,
+      })
+    );
+
+    // Routes
+    app.use(API_PREFIX, routes(dependencies));
+
+    // Common Error handler
     app.listen(PORT, () => {
       console.log(`WOHOOO our server is running under port ${PORT}`);
     });
